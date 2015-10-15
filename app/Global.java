2ad2d8b93a93e6a8785511f3cf70caa489f22835
Daniel Nesbitt <dan.nesbitt@gmail.com>
2015-10-15 07:37:34 -0400

    We'll need to do the initial service starts here.

---
+++ app/Global.java
@@ -0,0 +1,13 @@
+import play.Application;
+import play.GlobalSettings;
+
+/**
+ * @author Daniel Nesbitt
+ */
+public final class Global extends GlobalSettings {
+
+    @Override
+    public void beforeStart(Application app) {
+    }
+
+}
