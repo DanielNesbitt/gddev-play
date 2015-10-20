44d0f482e1248c3af45335c505e677714ed37438
James Cooper <james.cooper@genedata.com>
2015-10-20 09:24:53 +0200

    - Return removals - Add method for output to be converted to json

--- app/util/StatBag.java
+++ app/util/StatBag.java
@@ -1,9 +1,7 @@
 package util;
 
 import java.lang.Integer;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 import static java.util.Collections.nCopies;
@@ -23,18 +21,35 @@ public final class StatBag {
 
 	// -------------------- Public --------------------
 
-	public final void add(Map<String, Integer> tweets) {
+	public final List<String> add(Map<String, Integer> tweets) {
+        List<String> removals = new ArrayList<>();
 		tweets.forEach((k,v) -> push(counts.computeIfAbsent(k, s -> new ArrayDeque<>(nCopies(LIMIT, 0))), v));
 		counts.entrySet().removeIf(e -> {
 			if (!tweets.containsKey(e.getKey())) {
 				push(e.getValue(), 0);
-				return !e.getValue().stream().filter(i -> i > 0).findAny().isPresent();
+                if (!e.getValue().stream().filter(i -> i > 0).findAny().isPresent()) {
+                    removals.add(e.getKey());
+                    return true;
+                }
 			}
 			return false;
 		});
-	}
+        return removals;
+    }
 
-	private void push(Deque<Integer> deque, int value) {
+    public final Object[] columns() {
+        return counts.entrySet().stream()
+            .map(e -> {
+                List<Object> column = new ArrayList<>();
+                column.add(e.getKey());
+                column.addAll(e.getValue());
+                return column.toArray();
+            }).toArray();
+    }
+
+    // -------------------- Private --------------------
+
+    private void push(Deque<Integer> deque, int value) {
 		deque.add(value);
 		deque.removeFirst();
 	}
